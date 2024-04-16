bd4217338229d4bd6ac9bdb9d2840b4b90199b0b
Austin <austinngugi000@gmail.com>
2024-04-16 10:05:16 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(res => res.json())
+    .then(books => {
+      renderBooks(books);
+      return books; 
+    });
 }
 
 function renderBooks(books) {
@@ -13,5 +18,14 @@ function renderBooks(books) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+  fetchBooks().then(books => {
+    const fifthBook = books[4].name;
+    console.log("The 5th book in the series:", fifthBook);
+
+    const characterCount = books.reduce((totalChars, book) => totalChars + book.characters.length, 0);
+    const character1031 = characterCount >= 1031 ? books.find(book => book.characters.length >= 1031).name : "Not found";
+    console.log("The 1031st character in the series:", character1031);
+    const totalPages = books.reduce((total, book) => total + book.numberOfPages, 0);
+    console.log("The total number of pages of all the books:", totalPages);
+  });
 });
